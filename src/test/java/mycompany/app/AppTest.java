7001943039a48bdb51c4d32598001ca0eec93e39
Leong Shu Min <Leongshumin1@gmail.com>
2023-11-13 21:55:01 +0800

    Update AppTest.java

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://localhost:50000";
+	String url = "http://172.17.0.1:80";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
